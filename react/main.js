e9794438b98c0fd4de48f3680980f43a6eee77c1
luciano brocchi <104037458+elgufy@users.noreply.github.com>
2023-06-22 20:51:59 -0300

    Add files via upload

---
+++ react/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from './src/components/navbar.js';
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ react/src/components/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#">Categoría 1</a></li>
+        <li><a href="#">Categoría 2</a></li>
+        <li><a href="#">Categoría 3</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
